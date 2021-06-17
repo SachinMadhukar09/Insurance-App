f36c8913fc943b586e165a3eb27cdabe7188e4c8
Atulpal9045 <52998713+Atulpal9045@users.noreply.github.com>
2021-06-17 21:58:19 +0530

    Update Microproduct3.js

--- src/pages/Allmicroproducts/Microproduct3.js
+++ src/pages/Allmicroproducts/Microproduct3.js
@@ -20,10 +20,7 @@ const Microproduct3 = () => {
               <input type="text" className="content-input" />
               <label className="content-label">Phone Number</label>
               <input type="text" className="content-input" />
-              {/* <label className="content-label">Verify OTP</label> */}
-
-              {/* <input type="text" className="content-input" /> */}
-
+             
               <button
                 type="submit"
                 className="login-submit"
@@ -33,11 +30,7 @@ const Microproduct3 = () => {
                 {"Next"}
               </button>
             </form>
-            {/* <span>
-              {" "}
-              <p>Don't Get OTP</p>
-              <p style={{ color: "blue" }}>Resend</p>
-            </span> */}
+           
           </div>
         </div>
         <div className="section2">
@@ -83,48 +76,7 @@ const Microproduct3 = () => {
 
         <p>valid for 1 month</p>
 
-        {/* <button
-          type="submit"
-          className="login-submit"
-          // onClick={}
-          style={{ width: 150, marginRight: 20 }}
-        >
-          {"Buy Now"}
-        </button> */}
-      </div>
-      {/* <div className="header-top">
-        <div className="logo-img">
-           <img src={""} />
-        </div>
-        <div>
-            <h1>Product Name</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-      </div>
-      <div className="options">
-          {/* <ul>
-              <li><button>Option1</button></li>
-              <li><button>Option2</button></li>
-              <li><button>Option3</button></li>
-              <li><button>Option4</button></li>
-              <li><button>Option5</button></li>
-          </ul> 
-      </div>
-      <div>
-          <div className="content-body">
-              <form>
-                  <label></label>
-                  <input type="text"></input>
-                  <label></label>
-                  <input type="text"></input>
-                  <label></label>
-                  <input type="text"></input>
-                  <label></label>
-                  <input type="text"></input>
-              </form>
-          </div>
-          <div className="price-box"><div className="price"></div></div>
-      </div> */}
+       
       </div>
     </div>
   );
